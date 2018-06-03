1996947b7013ba683843f4058f43344a563213f7
Julien Sulpis <julien.sulpis@gmail.com>
2018-06-03 23:36:40 +0200

    fix: unexpected behaviour when clicking on a tab

--- src/js/material-tabs.js
+++ src/js/material-tabs.js
@@ -9,27 +9,25 @@ var dynamicTabBar = (window.dynamicTabBar = new MDCTabBar(
   document.querySelector("#dynamic-tab-bar")
 ));
 
-// dynamicTabBar.tabs.forEach(function(tab) {
-//   tab.preventDefaultOnClick = true;
-// });
-
-dynamicTabBar.listen("MDCTabBar:change", function({ detail: tabs }) {
-  var nthChildIndex = tabs.activeTabIndex;
-
-  //updatePanel(nthChildIndex);
-  //updateDot(nthChildIndex);
-});
+// "Debouncer" to freeze the active tab when
+// clicking on a tab
+var tabClickedTime;
+var tabs = document.getElementsByClassName("mdc-tab");
+for (var i = 0; i < tabs.length; i++) {
+  console.log(tabs[i]);
+  tabs[i].addEventListener("click", function() {
+    console.log("elt clicked");
+    tabClickedTime = new Date().getTime();
+  });
+}
 
 // STICKY TAB BAR
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {
-  myFunction();
-};
-
-// Get the header
 var navbar = document.getElementById("dynamic-toolbar");
 var firstSection = document.getElementById("experiences");
 var profile = document.getElementById("profile");
+var studies = document.getElementById("studies");
+var skills = document.getElementById("skills");
+var certifications = document.getElementById("certifications");
 
 function convertRemToPixels(rem) {
   return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
@@ -38,9 +36,12 @@ function convertRemToPixels(rem) {
 // Get the offset position of the navbar
 var sticky = profile.offsetHeight + convertRemToPixels(10) + 26 - 48;
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+// Add the sticky class to the header when we reach its scroll position. Remove "sticky" when we leave the scroll position.
+var offsetY;
+window.onscroll = function() {
+  offsetY = window.pageYOffset;
+  // Set the sticky tab bar
+  if (offsetY >= sticky) {
     navbar.classList.add("sticky");
     navbar.classList.add("mdc-elevation--z4");
     firstSection.style.paddingTop = "96px";
@@ -49,4 +50,20 @@ function myFunction() {
     navbar.classList.remove("mdc-elevation--z4");
     firstSection.style.paddingTop = "48px";
   }
-}
+
+  if (new Date().getTime() - tabClickedTime > 1000) {
+    // update the active tab
+    if (offsetY < sticky) {
+      dynamicTabBar.activeTabIndex = 0;
+    } else if (offsetY < studies.offsetTop) {
+      dynamicTabBar.activeTabIndex = 1;
+    } else if (offsetY < skills.offsetTop) {
+      dynamicTabBar.activeTabIndex = 2;
+    } else if (offsetY < certifications.offsetTop) {
+      dynamicTabBar.activeTabIndex = 3;
+    } else {
+      dynamicTabBar.activeTabIndex = 4;
+    }
+  }
+};
+
