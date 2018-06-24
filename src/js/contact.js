697a9c5c6ce7ce33dbc5a370d05e540863de762a
Julien Sulpis <julien.sulpis@gmail.com>
2018-06-25 00:01:12 +0200

    feat: send e-mail from formular (AWS back-end ready)

--- src/js/contact.js
+++ src/js/contact.js
@@ -12,9 +12,12 @@ document.querySelectorAll(".mdc-text-field").forEach(function(elt) {
 
 new MDCTextFieldIcon(document.querySelector(".mdc-text-field__icon"));
 
-/**
- * E-mail verification
- */
+import { MDCSnackbar } from "@material/snackbar";
+
+const snackbarElt = document.querySelector(".mdc-snackbar");
+const snackbar = new MDCSnackbar(snackbarElt);
+
+// E-mail verification
 document.getElementById("form-email").addEventListener("blur", function(elt) {
   var mail = elt.target.value;
   var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -30,9 +33,7 @@ document.getElementById("form-email").addEventListener("blur", function(elt) {
   }
 });
 
-/**
- * Name verification
- */
+// Name verification
 document.getElementById("form-name").addEventListener("blur", function(elt) {
   var name = elt.target.value;
   var nameField = document.getElementById("name-field");
@@ -50,21 +51,21 @@ document.getElementById("form-name").addEventListener("blur", function(elt) {
   }
 });
 
-/**
- * Message character count
- */
+// Message character count
 displayCharacterCount(0);
-document.getElementById("form-message").addEventListener("focus", function(elt) {
+document
+  .getElementById("form-message")
+  .addEventListener("focus", function(elt) {
     displayCharacterCount(elt.target.value.length);
-});
+  });
 
-document.getElementById("form-message").addEventListener("keyup", function(elt) {
+document
+  .getElementById("form-message")
+  .addEventListener("keyup", function(elt) {
     displayCharacterCount(elt.target.value.length);
-});
+  });
 
-/**
- * Message verification
- */
+// Message verification
 document.getElementById("form-message").addEventListener("blur", function(elt) {
   var message = elt.target.value;
   var messageField = document.getElementById("message-field");
@@ -87,32 +88,91 @@ document.getElementById("form-message").addEventListener("blur", function(elt) {
   }
 });
 
-/**
- * Form sending
- */
-document.querySelector('#contact-form').addEventListener("submit", function(elt) {
-    if (document.querySelectorAll('.mdc-text-field--invalid').length > 0) {
-        console.log("Invalid form");
+// Form sending
+document
+  .querySelector("#contact-form")
+  .addEventListener("submit", function(elt) {
+    if (document.querySelectorAll(".mdc-text-field--invalid").length > 0) {
+      console.log("Invalid formular");
+      displayErrorMessage("Formulaire invalide.");
     } else {
-        console.log("success");
-        // TODO: send email
+      console.log("Valid formular. Sending an e-mail...");
+      //console.log(getContactFormContent());
+      ajaxPost(
+        "https://90y1fzl9ij.execute-api.eu-west-3.amazonaws.com/prod",
+        getContactFormContent(),
+        function(response) {
+          console.log("E-mail sent. Response: " + response);
+          snackbarElt.classList.remove("invalid");
+          snackbar.show({ message: "Message envoyé !" });
+        }
+      );
     }
-});
+  });
 
-/**
- * Utils
- */
+// Returns a JSON object containing the formular content.
+function getContactFormContent() {
+  var name = document.querySelector("#form-name").value;
+  var email = document.querySelector("#form-email").value;
+  var message = document.querySelector("#form-message").value;
+  return {
+    name: name,
+    email: email,
+    message: message
+  };
+}
+
+// Makes an AJAX POST request
+function ajaxPost(url, data, callback) {
+  var req = new XMLHttpRequest();
+  req.open("POST", url);
+
+  req.addEventListener("load", function() {
+    if (req.status >= 200 && req.status < 400) {
+      // success, call the callback method and give it the response value
+      callback(req.responseText);
+    } else {
+      console.error(
+        req.status +
+          " : " +
+          req.statusText +
+          " from " +
+          url +
+          " - response: " +
+          req.responseText
+      );
+      displayErrorMessage("Une erreur est survenue. Veuillez réessayer.");
+    }
+  });
+
+  req.addEventListener("error", function() {
+    console.error("Network error with url: " + url);
+    displayErrorMessage("Une erreur est survenue. Veuillez réessayer.");
+  });
+
+  req.setRequestHeader("Content-Type", "application/json");
+  data = JSON.stringify(data);
+
+  req.send(data);
+}
+
+function displayErrorMessage(msg) {
+  snackbarElt.classList.add("invalid");
+  snackbar.show({ message: msg });
+}
+
+// Utils
 function setInvalid(elt) {
   elt.classList.add("mdc-text-field--invalid");
 }
 
 function distinctChars(str) {
-  // return the number of distinct characters in the string
+  // returns the number of distinct characters in the string
   return Array.from(new Set(str.split(""))).length;
 }
 
 function displayCharacterCount(charNumber) {
-    var messageErrorMsg = document.querySelector("#message-helper-text");
-    messageErrorMsg.classList.remove("message-error");
-    messageErrorMsg.textContent = charNumber + "/2048";
+  var messageErrorMsg = document.querySelector("#message-helper-text");
+  messageErrorMsg.classList.remove("message-error");
+  messageErrorMsg.textContent = charNumber + "/2048";
 }
